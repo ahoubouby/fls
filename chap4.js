9dd2086f6a7fd569075cc0752fa6c0bcf2c94a5b
ahoubouby <ahoubouby@gmail.com>
2020-09-15 20:50:48 +0100

    chap4: use reduce to avoid using let

--- chap4.js
+++ chap4.js
@@ -1,8 +1,16 @@
+const separator = "-------------------------------------------------";
+const printOutputCode = (codeReturn) => {
+  console.log(separator);
+  console.log(codeReturn);
+  console.log(separator);
+};
+
 const words = (str) =>
   String(str)
     .toLowerCase()
     .split(/\s|\b/)
     .filter((v) => /^[\w]+$/.test(v));
+
 function unique(list) {
   var uniqList = [];
   for (let v of list) {
@@ -14,8 +22,17 @@ function unique(list) {
   return uniqList;
 }
 
+const unique2 = (list) => {
+  return list.reduce((acc, cv) => {
+    if (acc && acc.indexOf(cv) === -1) return [...acc, cv];
+    return acc;
+  }, []);
+};
+
 const text =
   "To compose two functions together, pass the  output of the first function call as the input of the  second function call.";
 const wordsFound = words(text);
 const wordsUsed = unique(wordsFound);
+const wordsUsed2 = unique2(wordsFound);
 console.log(wordsUsed);
+printOutputCode(wordsUsed2);
