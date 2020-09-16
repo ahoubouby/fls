5940dfc56f8487cb70169ecaaf8f94a2e5f83e98
ahoubouby <ahoubouby@gmail.com>
2020-09-16 13:59:12 +0100

    chap4 : Compose for many functions we like

--- chap4.js
+++ chap4.js
@@ -41,3 +41,19 @@ const wordsUsed = unique(wordsFound);
 const wordsUsed2 = unique2(wordsFound);
 console.log(wordsUsed);
 printOutputCode(wordsUsed2);
+
+const letters = compose2(words, unique);
+const letters2 = compose2(unique, words);
+const chars = letters("How are you Henry?");
+const chars2 = letters2("How are you Henry?");
+printOutputCode(chars);
+printOutputCode(chars2);
+
+//------ compose
+
+const composes = (...fns) => (x) => fns.reduceRight((y, f) => f(y), x);
+
+const letters3 = composes(unique, words);
+const chars3 = letters3("How are you Henry?");
+console.log("char3");
+printOutputCode(chars3);
