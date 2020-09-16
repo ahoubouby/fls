4f9b05056f190d389bb6e5e15e297ff03f7b15e7
ahoubouby <ahoubouby@gmail.com>
2020-09-16 16:05:40 +0100

    chap4 : pipe functions using reduce

--- chap4.js
+++ chap4.js
@@ -1,4 +1,5 @@
 const separator = "-------------------------------------------------";
+
 const printOutputCode = (codeReturn) => {
   console.log(separator);
   console.log(codeReturn);
@@ -6,12 +7,17 @@ const printOutputCode = (codeReturn) => {
 };
 //
 
+const partial = (fn, ...presetArgs) => (...laterArgs) =>
+  fn(...presetArgs, ...laterArgs);
+
 const text =
   "To compose two functions together, pass the  output of the first function call as the input of the  second function call.";
 // utility function
 const compose2 = (fn2, fn1) => (orignValue) => fn2(fn1(orignValue));
 
 const composes = (...fns) => (x) => fns.reduceRight((y, f) => f(y), x);
+
+const pipe = (...fns) => (x) => fns.reduce((y, f) => f(y), x);
 
 //------ compose
 
@@ -38,17 +44,19 @@ const wordsUsed = unique(wordsFound);
 
 console.log(wordsUsed);
 
-const letters = compose2(words, unique);
-const letters2 = compose2(unique, words);
+// take care of args
+const letters = compose2(unique, words);
 const chars = letters("How are you Henry?");
-const chars2 = letters2("How are you Henry?");
 printOutputCode(chars);
-printOutputCode(chars2);
 
 const letters3 = composes(unique, words);
 const chars3 = letters3("How are you Henry?");
 console.log("char3");
 printOutputCode(chars3);
-const letters4 = composes(skipShortWords, unique, words);
-const chars4 = letters4(text);
-printOutputCode(chars4);
+// const letters4 = composes(skipShortWords, unique, words);
+// const chars4 = letters4(text);
+// printOutputCode(chars4);
+
+const letter5 = pipe(words, unique, skipShortWords);
+const chars5 = letter5("how are you you you henry henry ?");
+printOutputCode(chars5);
