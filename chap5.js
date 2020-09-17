d00ea9efc7c8d521204b59ef0be51ecfea068cee
ahoubouby <ahoubouby@gmail.com>
2020-09-17 21:50:21 +0100

    🎉 Chapter 5: Reducing Side Effects
    init commit

--- chap5.js
+++ chap5.js
@@ -1,1 +1,17 @@
 //Chapter 5: Reducing Side Effects
+const updateCounter = (obj) => {
+  if (obj.count < 10) {
+    obj.count++;
+    return true;
+  }
+  return false;
+};
+
+function currency(val) {
+  var num = parseFloat(String(val).replace(/[^\d.-]+/g, ""));
+  var sign = num < 0 ? "-" : "";
+  return `${sign}$${Math.abs(num).toFixed(3)}`;
+}
+
+console.log(currency(-3.1));
+console.log(currency(currency(-3.1)));
